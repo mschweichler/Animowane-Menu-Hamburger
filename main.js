3d2af1972c804a378c8fd0d0cd2cdccba6d36a30
michal.schweichler <michal.schweichler@gmail.com>
2020-06-07 15:30:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const menuBtn = document.querySelector('.menu-btn');
+let isMenuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+    if(!isMenuOpen) {
+        menuBtn.classList.add('open');
+        isMenuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        isMenuOpen = false;
+    }
+});
